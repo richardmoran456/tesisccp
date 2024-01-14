82ce72f9e935509fa45fd30403c11ea9b5408e1d
José Heredia <jochdev06@gmail.com>
2024-01-14 10:12:32 -0400

    Estructura de vistas
    Se crearon las bases de los apartados en la aplicación

--- vistas/paginas/usuarios-view.php
+++ vistas/paginas/usuarios-view.php
@@ -1,35 +1,35 @@
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Usuarios del sistema</h1>
-            <p >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam dicta id perspiciatis adipisci sint tenetur possimus consectetur facilis vero dolor? Accusantium optio laudantium vel natus magnam repudiandae repellat. Maiores, quas?</p>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo SERVERURL . "home" ?>">Dashboard</a></li>
-              <li class="breadcrumb-item active">Usuarios</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Usuarios del sistema</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam dicta id perspiciatis adipisci sint tenetur possimus consectetur facilis vero dolor? Accusantium optio laudantium vel natus magnam repudiandae repellat. Maiores, quas?</p>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?php echo SERVERURL . "home" ?>">Dashboard</a></li>
+            <li class="breadcrumb-item active">Usuarios</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-6 mb-4">
-          <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">Nuevo Usuario</button>
-          </div>
-        </div>
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
       <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <!-- <div class="card-header">
+        <div class="col-6 mb-4">
+          <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">Nuevo Usuario</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <!-- <div class="card-header">
                 <h3 class="card-title">Responsive Hover Table</h3>
 
                 <div class="card-tools">
@@ -44,80 +44,80 @@
                   </div>
                 </div>
               </div> -->
-              <!-- /.card-header -->
-              <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>User</th>
-                      <th>Date</th>
-                      <th>Status</th>
-                      <th>Reason</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>183</td>
-                      <td>John Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-success">Approved</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                      <td>219</td>
-                      <td>Alexander Pierce</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-warning">Pending</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                      <td>657</td>
-                      <td>Bob Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-primary">Approved</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                      <td>175</td>
-                      <td>Mike Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-danger">Denied</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+              <table class="table table-hover text-nowrap">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>Reason</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  </tr>
+                  <tr>
+                    <td>219</td>
+                    <td>Alexander Pierce</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-warning">Pending</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  </tr>
+                  <tr>
+                    <td>657</td>
+                    <td>Bob Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-primary">Approved</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  </tr>
+                  <tr>
+                    <td>175</td>
+                    <td>Mike Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-danger">Denied</span></td>
+                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
           </div>
+          <!-- /.card -->
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-
-    <!-- /.content -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
 
+  <!-- /.content -->
+</div>
 
-  <!-- modal  -->
-  <div class="modal fade" id="modal-default">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Crear usuario</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>One fine body&hellip;</p>
-        </div>
-        <div class="modal-footer justify-content-between">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
+
+<!-- modal  -->
+<div class="modal fade" id="modal-default">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Crear usuario</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
+</div>
--- vistas/tema/aside.php
+++ vistas/tema/aside.php
@@ -1,24 +1,24 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="<?php echo SERVERURL . "home" ?>" class="brand-link">
-      <img src="<?php SERVERURL;?>vistas/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"><?php echo COMPANY; ?></span>
-    </a>
+  <!-- Brand Logo -->
+  <a href="<?php echo SERVERURL . "home" ?>" class="brand-link">
+    <img src="<?php SERVERURL; ?>vistas/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light"><?php echo COMPANY; ?></span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?php SERVERURL;?>vistas/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="<?php echo SERVERURL . "perfil" ?>" class="d-block">Nombre</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="<?php SERVERURL; ?>vistas/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
+      <div class="info">
+        <a href="<?php echo SERVERURL . "perfil" ?>" class="d-block">Nombre</a>
+      </div>
+    </div>
 
-      <!-- SidebarSearch Form -->
-      <!-- <div class="form-inline">
+    <!-- SidebarSearch Form -->
+    <!-- <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
           <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
           <div class="input-group-append">
@@ -29,12 +29,12 @@
         </div>
       </div> -->
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <!-- <li class="nav-item menu-open">
+        <!-- <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
@@ -120,54 +120,92 @@
             </a>
           </li> -->
 
-          <!-- menu app -->
-          <li class="nav-item">
-            <a href="<?php echo SERVERURL . "usuarios" ?>" class="nav-link">
-              <p>Usuarios</p>
-            </a>
-          </li>
+        <!-- menu app -->
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "usuarios" ?>" class="nav-link">
+            <p>Usuarios</p>
+          </a>
+        </li>
 
-          <li class="nav-item">
-            <a href="<?php echo SERVERURL . "solicitudes" ?>" class="nav-link">
-              <p>Solicitudes</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="<?php echo SERVERURL . "tareas" ?>" class="nav-link">
-              <p>Tareas</p>
-            </a>
-          </li>
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "eventos" ?>" class="nav-link">
+            <p>Eventos</p>
+          </a>
+        </li>
 
-          <li class="nav-item">
-            <a href="<?php echo SERVERURL . "huespedes" ?>" class="nav-link">
-              <p>Huéspedes</p>
-            </a>
-          </li>
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "equipos" ?>" class="nav-link">
+            <p>Equipos</p>
+          </a>
+        </li>
 
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <p>
-                Hotel
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "alas" ?>" class="nav-link">
+            <p>Alas</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "empleados" ?>" class="nav-link">
+            <p>Empleados</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "departamentos" ?>" class="nav-link">
+            <p>Departamentos</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "puestos" ?>" class="nav-link">
+            <p>Puestos</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "solicitudes" ?>" class="nav-link">
+            <p>Solicitudes</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "tareas" ?>" class="nav-link">
+            <p>Tareas</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="<?php echo SERVERURL . "huespedes" ?>" class="nav-link">
+            <p>Huéspedes</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <p>
+              Hotel
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
             <li class="nav-item">
-                <a href="<?php echo SERVERURL . "pisos" ?>" class="nav-link">
-                  <p>Pisos</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?php echo SERVERURL . "habitaciones" ?>" class="nav-link">
-                  <p>Habitaciones</p>
-                </a>
-              </li>
+              <a href="<?php echo SERVERURL . "pisos" ?>" class="nav-link">
+                <p>Pisos</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?php echo SERVERURL . "habitaciones" ?>" class="nav-link">
+                <p>Habitaciones</p>
+              </a>
+            </li>
 
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+          </ul>
+        </li>
+
+
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
