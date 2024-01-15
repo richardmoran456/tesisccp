9d3f3588ae006da5a621033a1bb395734d72d083
José Heredia <jochdev06@gmail.com>
2024-01-15 11:16:17 -0400

    Update SERVER.php

--- config/SERVER.php
+++ config/SERVER.php
@@ -1,11 +1,11 @@
 <?php
 const SERVER = "localhost";
-const DB = "";
-const USER = "";
+const DB = "ccp";
+const USER = "root";
 const PASS = "";
 
 const SGBD = "mysql:host=" . SERVER . ";dbname=" . DB;
 
 const METHOD = "AES-256-CBC";
 const SECRET_KEY = '$mortadela20232';
-const SECRET_ID = "02354";
+const SECRET_IV = "02354";
