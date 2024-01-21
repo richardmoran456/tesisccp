2b7fe63acc422a3b5a569ec9942401f91642447b
richardmoran456 <richardmoran456@gmail.com>
2024-01-20 23:21:34 -0400

    actualizar equipo
    se arreglo el error al actualizar que no tenia todos los campos

--- vistas/paginas/equipo-update-view.php
+++ vistas/paginas/equipo-update-view.php
@@ -47,17 +47,29 @@ if ($datos_modulo->rowCount() == 1) {
 
 
                             <form class="FormularioAjax" action="<?php echo SERVERURL; ?>ajax/equipoAjax.php" method="POST" data-form="update" autocomplete="off">
-                                <input type="hidden" name="departamento_id_up" value="<?php echo $pagina[1]; ?>">
-
+                                <input type="hidden" name="equipo_id_up" value="<?php echo $pagina[1]; ?>">
 
                                 <div class="form-group">
-                                    <label for="nombre_departamento_up">Nombre departamento</label>
-                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" id="nombre_departamento_up" placeholder="Ingresa el nombre" maxlength="35" required="" name="nombre_departamento_up" value="<?php echo $campos['nombre']; ?>">
+                                    <label for="modelo_equipo_up">Modelo del Equipo</label>
+                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" id="modelo_equipo_up" placeholder="Ingresa el modelo" maxlength="35" required="" name="modelo_equipo_up">
                                 </div>
                                 <div class="form-group">
-                                    <label for="abreviatura_departamento_up">abreviatura</label>
-                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,10}" class="form-control" id="abreviatura_departamento_up" placeholder="Ingresa la abreviatura" maxlength="10" required="" name="abreviatura_departamento_up" value="<?php echo $campos['abreviatura']; ?>">
+                                    <label for="nserial_equipo_up">Serial</label>
+                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,10}" class="form-control" id="nserial_equipo_up" placeholder="Ingrese el serial del equipo" maxlength="10" required="" name="nserial_equipo_up">
                                 </div>
+                                <div class="form-group">
+                                    <label for="estado_equipo_up">Estado</label>
+                                    <input type="text" class="form-control" id="estado_equipo_up" placeholder="Estadooooo" maxlength="35" required="" name="estado_equipo_up">
+                                </div>
+                                <div class="form-group">
+                                    <label for="descripcion_equipo_up">Descripcion</label>
+                                    <input type="text" class="form-control" id="descripcion_equipo_up" placeholder="Ingrese una breve descripcion del equipo" maxlength="35" required="" name="descripcion_equipo_up">
+                                </div>
+                                <div class="form-group">
+                                    <label for="tipo_equipo_up">Tipo</label>
+                                    <input type="text" class="form-control" id="tipo_equipo_up" placeholder="Indique el tipo de equipo" maxlength="35" required="" name="tipo_equipo_up">
+                                </div>
+
 
 
                                 <button type="submit" class="btn btn-primary">Actualizar equipo</button>
