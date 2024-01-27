7c94f81c640ccef11c025cdb20436b5fe19f3db2
richardmoran456 <richardmoran456@gmail.com>
2024-01-27 12:36:26 -0400

    Fix: leyenda en cambiar foto

--- vistas/paginas/perfil-view.php
+++ vistas/paginas/perfil-view.php
@@ -50,7 +50,7 @@ if ($datos_modulo->rowCount() == 1) {
           <!-- /.card -->
           <div class="card card-primary">
             <div class="card-header">
-              <h3 class="card-title">Cambiar contraseña</h3>
+              <h3 class="card-title">Cambiar Foto de Perfil</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
