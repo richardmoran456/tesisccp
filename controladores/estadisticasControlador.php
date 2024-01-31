743e7eefe0769fd9068f13d25f1a3d0685b77c0d
José Heredia <jochdev06@gmail.com>
2024-01-31 13:09:45 -0400

    Base estadisticas

---
+++ controladores/estadisticasControlador.php
@@ -0,0 +1,19 @@
+<?php
+
+if ($peticionAjax) {
+	require_once "../modelos/estadisticaModelo.php";
+} else {
+	require_once "./modelos/estadisticaModelo.php";
+}
+
+class estadisticasControlador extends estadisticaModelo
+{
+
+
+	/*--------- Mostrar cantidad de usuarios registrados ---------*/
+	public function show_total_usuarios()
+	{
+		$datos = estadisticaModelo::cantidad_usuarios_registrados();
+		return $datos;
+	}
+}
--- vistas/paginas/home-view.php
+++ vistas/paginas/home-view.php
@@ -1,3 +1,35 @@
+<?php
+//  FIXME Repasar como se estructura una consulta SLQ para obtener el numero de filas de una tabla 
+// https://lineadecodigo.com/sql/contar-el-numero-de-registros-en-sql/
+
+/**
+ * Tenemos un controlador y modelo para las estadisticas
+ * por eso realizamos solamente una instancia del mismo y lo que cambiara son las funciones 
+ * que llamaremos 
+ *  
+ * Hice una de ejemplo, donde mostramos la cantidad de usuarios en el sistema.
+ * 
+ * QUE HACER PRIMERO?
+ * 1. Identificar la card en la que trabajaras. 
+ *  En mi ejemplo ubique la card de usuarios registrados.
+ * 2. En el controlador cree una funcion llamada show_total_usuarios()
+ *  En esa funcion del controlador estoy invocando una funcion del modelo 
+ * 3. La funcion del modelo es la que tiene la conexion a la base de datos
+ *  En esta funcion cree una sentencia SQL que me da la cantidad de filas que tiene esa tabla.
+ *  Si no manejas esos conceptos aqui esta lo basico https://lineadecodigo.com/sql/contar-el-numero-de-registros-en-sql/
+ *  En la funcion del modelo solo vas a cambiar el nombre de la tabla.
+ * en caso de que se vaya a necesitar comparar un atributo de la tabla usa el WHERE en la sentencia SQL
+ * 
+ * 
+ * EL FLUJO ES VISTA-CONTROLADOR-MODELO ESO QUIERE DECIR QUE DEBES SEGUIR LOS PASOS DESCRITOS ACA.
+ */
+
+
+require_once "./controladores/estadisticasControlador.php";
+$ins_controlador = new estadisticasControlador();
+
+?>
+
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <div class="content-header">
@@ -22,13 +54,13 @@
     <div class="container-fluid">
       <div class="row">
 
-      <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-3">
           <div class="info-box mb-3">
             <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
             <div class="info-box-content">
               <span class="info-box-text">Usuarios registrados</span>
-              <span class="info-box-number">2,000</span>
+              <span class="info-box-number"><?php echo  $ins_controlador->show_total_usuarios(); ?></span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -80,7 +112,7 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-       
+
         <!-- /.col -->
       </div>
       <!-- /.row -->
@@ -93,7 +125,7 @@
     <div class="container-fluid">
       <div class="row">
 
-      <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-3">
           <div class="info-box mb-3">
             <span class="info-box-icon bg-warning elevation-1"><i class="fa fa-wallet"></i></span>
 
@@ -152,7 +184,7 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-       
+
         <!-- /.col -->
       </div>
       <!-- /.row -->
@@ -165,7 +197,7 @@
     <div class="container-fluid">
       <div class="row">
 
-      <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-3">
           <div class="info-box mb-3">
             <span class="info-box-icon bg-success elevation-1"><i class="fa fa-thumbs-up"></i></span>
             <div class="info-box-content">
@@ -205,7 +237,7 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-      
+
         <!-- /.col -->
 
         <!-- fix for small devices only -->
@@ -214,7 +246,7 @@
         <div class="col-12 col-sm-6 col-md-3">
           <div class="info-box mb-3">
             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-sitemap"></i></span>
-        
+
             <div class="info-box-content">
               <span class="info-box-text">Total departamentos</span>
               <span class="info-box-number">760</span>
@@ -224,7 +256,7 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-       
+
         <!-- /.col -->
       </div>
       <!-- /.row -->
