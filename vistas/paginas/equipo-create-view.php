a699fe5254a16609b630ece3265cefc3d469dbc5
richardmoran456 <richardmoran456@gmail.com>
2024-01-31 14:55:54 -0400

    Fix: Estados en crear equipo
    se creo el select en crear equipos

--- vistas/paginas/equipo-create-view.php
+++ vistas/paginas/equipo-create-view.php
@@ -30,31 +30,46 @@
                             <h5 class="card-title">Formulario de Registro para un equipo</h5>
 
                             <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nihil praesentium quod pariatur accusamus temporibus, illum blanditiis id dignissimos commodi ullam perspiciatis! Voluptatum sunt provident vel ratione nesciunt? Dolorum, inventore?
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nihil praesentium quod
+                                pariatur accusamus temporibus, illum blanditiis id dignissimos commodi ullam
+                                perspiciatis! Voluptatum sunt provident vel ratione nesciunt? Dolorum, inventore?
                             </p>
 
 
 
-                            <form class="FormularioAjax" action="<?php echo SERVERURL; ?>ajax/equipoAjax.php" method="POST" data-form="save" autocomplete="off">
+                            <form class="FormularioAjax" action="<?php echo SERVERURL; ?>ajax/equipoAjax.php"
+                                method="POST" data-form="save" autocomplete="off">
                                 <div class="form-group">
                                     <label for="modelo_equipo_reg">Modelo del Equipo</label>
-                                    <input type="text" class="form-control" id="modelo_equipo_reg" placeholder="Ingresa el modelo" maxlength="35" required="" name="modelo_equipo_reg">
+                                    <input type="text" class="form-control" id="modelo_equipo_reg"
+                                        placeholder="Ingresa el modelo" maxlength="35" required=""
+                                        name="modelo_equipo_reg">
                                 </div>
                                 <div class="form-group">
                                     <label for="nserial_equipo_reg">Serial</label>
-                                    <input type="text" class="form-control" id="nserial_equipo_reg" placeholder="Ingrese el serial del equipo" maxlength="35" required="" name="nserial_equipo_reg">
+                                    <input type="text" class="form-control" id="nserial_equipo_reg"
+                                        placeholder="Ingrese el serial del equipo" maxlength="35" required=""
+                                        name="nserial_equipo_reg">
                                 </div>
                                 <div class="form-group">
                                     <label for="estado_equipo_reg">Estado</label>
-                                    <input type="text" class="form-control" id="estado_equipo_reg" placeholder="Estadooooo" maxlength="35" required="" name="estado_equipo_reg">
+                                    <select class="form-control" id="estado_equipo_reg" name="estado_equipo_reg">
+                                        <option value="funcionando">Funcionando</option>
+                                        <option value="dañado">Dañado</option>
+                                    </select>
                                 </div>
+
                                 <div class="form-group">
                                     <label for="descripcion_equipo_reg">Descripcion</label>
-                                    <input type="text" class="form-control" id="descripcion_equipo_reg" placeholder="Ingrese una breve descripcion del equipo" maxlength="35" required="" name="descripcion_equipo_reg">
+                                    <input type="text" class="form-control" id="descripcion_equipo_reg"
+                                        placeholder="Ingrese una breve descripcion del equipo" maxlength="35"
+                                        required="" name="descripcion_equipo_reg">
                                 </div>
                                 <div class="form-group">
                                     <label for="tipo_equipo_reg">Tipo</label>
-                                    <input type="text" class="form-control" id="tipo_equipo_reg" placeholder="Indique el tipo de equipo" maxlength="35" required="" name="tipo_equipo_reg">
+                                    <input type="text" class="form-control" id="tipo_equipo_reg"
+                                        placeholder="Indique el tipo de equipo" maxlength="35" required=""
+                                        name="tipo_equipo_reg">
                                 </div>
 
                                 <button type="submit" class="btn btn-primary">Registrar equipo</button>
