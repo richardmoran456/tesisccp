0b8f0009ecebcc993ec087dd5449f1fcd889252b
José Heredia <jochdev06@gmail.com>
2024-02-03 14:04:17 -0400

    feat: registro huesped y asignacion

--- ajax/gestorHabitacionAjax.php
+++ ajax/gestorHabitacionAjax.php
@@ -2,9 +2,9 @@
 $peticionAjax = true;
 require_once "../config/APP.php";
 
+// var_dump($_POST);
 
-
-if (isset($_POST['search']) || isset($_POST['fk_huesped_asignado'])) {
+if (isset($_POST['search']) || isset($_POST['fk_huesped_asignado']) || isset($_POST['nombre_completo_reg'])) {
     /*--------- Instancia al controlador ---------*/
     require_once "../controladores/gestorHabitacionControlador.php";
     $instancia_controlador = new gestorHabitacionControlador();
@@ -18,6 +18,12 @@ if (isset($_POST['search']) || isset($_POST['fk_huesped_asignado'])) {
     /*--------- Asignar huesped a una habitacion ---------*/
     if (isset($_POST['fk_huesped_asignado'])) {
         echo $instancia_controlador->asignar_huesped();
+    }
+
+    /*--------- Crear huesped y asignar a una habitacion ---------*/
+    if (isset($_POST['nombre_completo_reg'])) {
+
+        echo $instancia_controlador->crear_asignar_huesped();
     }
 } else {
     echo "aqui";
